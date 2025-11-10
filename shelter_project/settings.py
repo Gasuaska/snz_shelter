1bf95183e898536dc9dcb49da8f35221ec5c588d
Gasuaska <gasuaska2@gmail.com>
2025-11-11 00:28:55 +0500

    поменяла бд на постгрес

--- shelter_project/settings.py
+++ shelter_project/settings.py
@@ -71,8 +71,11 @@ WSGI_APPLICATION = 'shelter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
     }
 }
 
