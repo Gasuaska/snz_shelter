be0e2083788f65aa15c3f0d796811e330b566cca
Gasuaska <gasuaska2@gmail.com>
2026-03-18 13:21:44 +0500

    fixing workflow

--- shelter_project/settings.py
+++ shelter_project/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'static_collected'
 
 MEDIA_URL = '/media/'
