e1a83616dc66856212dc6a2867b801447eabd848
Gasuaska <gasuaska2@gmail.com>
2026-03-19 15:38:30 +0500

    migrations deleted

--- cats/apps.py
+++ cats/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cats'
+    
+    class Meta:
+        verbose_name='Кошки'
