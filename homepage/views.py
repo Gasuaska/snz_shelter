2c8238b475373ad566f8801e5cbbc85c02445274
Gasuaska <gasuaska2@gmail.com>
2026-03-23 22:17:21 +0500

    views fix

--- homepage/views.py
+++ homepage/views.py
@@ -9,11 +9,11 @@ from dogs.views import render_md
 
 
 def index(request):
-    dogs = list(DogInfo.objects.all())
+    dogs = list(DogInfo.objects.filter(is_at_shelter=True))
     random_dogs = random.sample(dogs, min(len(dogs), 3))
-    cats = list(CatInfo.objects.all())
+    cats = list(CatInfo.objects.filter(is_at_shelter=True))
     random_cats = random.sample(cats, min(len(cats), 3))
-    urgent = DOGS_URGENT
+    urgent = list(DogInfo.objects.filter(urgent=True))
     blog_list = Post.objects.order_by('-pub_date')[:3]
     for post in blog_list:
         post.html_text = render_md(post.text)
