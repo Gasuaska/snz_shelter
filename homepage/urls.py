e810f979cc03597e16e610991c79a17f96b1cc54
Gasuaska <gasuaska2@gmail.com>
2026-03-23 21:57:04 +0500

    views fix

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,4 +6,5 @@ app_name = 'homepage'
 
 urlpatterns = [
     path('', views.index, name='index'),
+        path('yandex_fd455f5dc9fae22f.html', TemplateView.as_view(template_name="yandex_fd455f5dc9fae22f.html")),
 ]
